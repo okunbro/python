cf2a36ab2f6246f4c522e1b09ccf1f4e1b8790fb
okunbro <okunbro@gmail.com>
2025-05-10 15:03:21 +0300

    Add files via upload

--- schemas.py
+++ schemas.py
@@ -12,9 +12,7 @@ class PlainStoreSchema(Schema):
     name = fields.Str()
 
 
-class ItemSchema(PlainItemSchema):
-    store_id = fields.Int(required=True, load_only=True)
-    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
 
 
 class ItemUpdateSchema(Schema):
@@ -35,4 +33,15 @@ class StoreSchema(PlainStoreSchema):
 
 class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
