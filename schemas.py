2d428ef5311ec2c927c24050e9cb6215a4091c8d
okunbro <okunbro@gmail.com>
2025-05-10 13:25:13 +0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -23,5 +23,16 @@ class ItemUpdateSchema(Schema):
     store_id = fields.Int()
 
 
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
